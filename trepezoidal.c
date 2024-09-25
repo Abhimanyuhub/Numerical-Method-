4f475bc0cb1a37fc355c6b4c9a8df5a7738144cf
Abhimanyuhub <manyu0623@gmail.com>
2024-09-25 14:51:32 +0530

    Update trepezoidal.c

--- trepezoidal.c
+++ trepezoidal.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 #include<math.h>
+
 // define the function
 float trapezoidal(float x){
-return x*x*x+1;
+    return x*x*x+1;
 }
+
 int main(){
-float a,b;       //a and b are upperlimit and lower limit
-int n,i;           //interval
-printf("enter the value of a & b: ");
-scanf("%f %f",&a,&b);
-printf("enter the value of n: ");
-scanf("%d",&n);
-float c = (a-b)/n;
-float integral = trapezoidal(b);
-if (i=1,i<n,i++){
- b=b+i*c;
- integral=integral+2*trapezoidal(b);
-}
- integral=integral+trapezoidal(a);
- integral=(c*integral)/2;
- printf("%f",integral);
+    float a, b;       // a and b are upper limit and lower limit
+    int n;           // interval
+    printf("enter the value of a & b: ");
+    scanf("%f %f",&a,&b);
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+
+    float h = (b - a) / n; // interval width
+    float sum = 0;
+    for (int i = 1; i < n; i++) {
+        float xi = a + i * h;
+        sum += 2 * trapezoidal(xi);
+    }
+   float ans = (h / 2) * (trapezoidal(a) + trapezoidal(b) + sum);
+    printf("%f", ans);
+    return 0;
 }
